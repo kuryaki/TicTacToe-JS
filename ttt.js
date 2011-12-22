33866422ee8eaa18078e34df6e1a3d4e61ac19f4
David Roncancio <david.roncancio@gmail.com>
2011-12-22 17:12:20 -0500

    first real commit, so far it draws the cell and do some interaction work session:4hours

---
+++ ttt.js
@@ -0,0 +1,99 @@
+function drawCanvas() {
+	canvas = document.getElementById('micanvas');
+	canvas.addEventListener("click", play, false);
+	cellsArray=new Array();
+	player=0;
+	
+	sideSize = canvas.width = canvas.height = window.innerWidth / 3;
+	cellSize=canvas.width/3;
+	
+	if (canvas.getContext) {
+		
+		ctx = canvas.getContext("2d");
+
+		for (i = 1; i < 3; i++) {
+			ctx.lineWidth = 2;
+
+			//Verticales
+			ctx.beginPath();
+			ctx.moveTo(i * cellSize, 0);
+			ctx.lineTo(i * cellSize, sideSize);
+			ctx.stroke();
+			ctx.closePath();
+
+			//Horizontales
+			ctx.beginPath();
+			ctx.moveTo(0, i * cellSize);
+			ctx.lineTo(sideSize, i * cellSize);
+			ctx.stroke();
+			ctx.closePath();
+		}
+	}
+}
+
+function play(e) {
+	
+	var cell = getClickedCell(e);
+	
+	cellsArray.forEach(function evaluateAvaliability(myCell){
+		if(cell.x==myCell.x && cell.y==myCell.y){
+			alert("NO!!");
+		}
+	});
+	
+	cellsArray.push(cell);
+	
+	if(player==0){
+		new EmptyCircle(cell);
+		player=1;
+	}else{
+		new FilledCircle(cell);
+		player=0;
+	}
+	
+	
+}
+
+function getClickedCell(e){
+	
+	var x;
+	var y;
+	if (e.pageX || e.pageY) {
+		x = e.pageX;
+		y = e.pageY;
+	} else {
+		x = e.clientX + document.body.scrollLeft
+				+ document.documentElement.scrollLeft;
+		y = e.clientY + document.body.scrollTop
+				+ document.documentElement.scrollTop;
+	}
+
+	x -= canvas.offsetLeft;
+	y -= canvas.offsetTop;
+	
+	var cell = new Cell(Math.floor(x/cellSize), Math.floor(y/cellSize));
+	
+	return cell;
+}
+
+function Cell(x, y){
+	this.x=x;
+	this.y=y;
+}
+
+function FilledCircle(cell){
+	
+	ctx.beginPath();
+	ctx.arc(cellSize/2+cell.x*cellSize,cellSize/2+cell.y*cellSize,cellSize/2,0,Math.PI+(Math.PI*3)/2,false);
+	ctx.fill();
+	ctx.closePath();
+	
+}
+function EmptyCircle(cell){
+	
+	ctx.beginPath();
+	ctx.arc(cellSize/2+cell.x*cellSize,cellSize/2+cell.y*cellSize,cellSize/2,0,Math.PI+(Math.PI*3)/2,false);
+	ctx.stroke();
+	ctx.closePath();
+	
+}
